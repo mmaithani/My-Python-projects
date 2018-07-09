d068a1e4697a9329a299cffcda2e5d3bddc01083
Mohit Maithani <mohitmaithani81@gmail.com>
2018-07-09 20:53:54 +0530

    update backend

--- 1.book_dictionary/backend.py
+++ 1.book_dictionary/backend.py
@@ -1,31 +1,31 @@
 import pymysql as py
 
-#taking databse username and password
+#----------Taking databse username and password-------------
 data=open("database_pass.txt","r")
 username=data.readline() 
 password=data.readline()
 
-#open database connection
+#----------open database connection---------------------------
 db=py.connect("localhost","root",password,"book_dic")
 
-#prepare a cursor object using cursor() method
+#--------prepare a cursor object using cursor() method----------
 cursor=db.cursor()
 
-cursor.execute("drop table book")
-# cursor.execute("drop table year")
-# cursor.execute("drop table ISBN")
-# cursor.execute("drop table author")
+#########################- Button commands -################################################
 
-book="create table book(title char(25),year int,ISBN char(20),author char(20))"
-# year="create table book(book_id int,title char(20),char(20),genre char(20))"
-# ISBN="create table publishers(publisher_id int,name char(20),street_add char(20),suit_number int,zip_code_id int)"
-# author="create table authors(author_id int,first_name char(20),middle_name char(20),last_name char(20))"
-print(cursor.execute("show tables"))
+def view_all():
+	cursor.execute("select * from book")
+	result=cursor.fetchall()
+def search():	
+	pass
+def add():
+	pass
+def update():
+	pass
+def delete():
+	pass
 
-cursor.execute(book)
-# cursor.execute(year)
-# cursor.execute(ISBN)
-# cursor.execute(author)
+add
 
 #disconnect from server
 db.close()
