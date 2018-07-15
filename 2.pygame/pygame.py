b42e0aecf1f34ce205b694cf722dd66160541ddc
Mohit Maithani <mohitmaithani81@gmail.com>
2018-07-15 22:39:10 +0530

    trying fb script

--- 2.pygame/pygame.py
+++ 2.pygame/pygame.py
@@ -1,1 +1,14 @@
-import pygame
+# import pygame
+import fbchat
+from getpass import getpass
+username = str(input("Username: "))
+client = fbchat.Client(username, getpass())
+no_of_friends = int(input("Number of friends: "))
+for i in xrange(no_of_friends):
+    name = str(input("Name: "))
+    friends = client.getUsers(name)  # return a list of names
+    friend = friends[0]
+    msg = str(input("Message: "))
+    sent = client.send(friend.uid, msg)
+    if sent:
+        print("Message sent successfully!")
