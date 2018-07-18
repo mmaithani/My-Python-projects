aa2728b6c090f9757f0258ab3f35f35105a008bc
Mohit Maithani <mohitmaithani81@gmail.com>
2018-07-18 18:23:05 +0530

    m

--- 2.pygame/snake.py
+++ 2.pygame/snake.py
@@ -1,1 +1,12 @@
-import django
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((800,300))
+pygame.display.set_caption('Mohit first pygame!')
+while True: # main game loop
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+	pygame.display.update()
