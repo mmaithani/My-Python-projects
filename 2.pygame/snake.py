366a6fc0e9590472f85b55acfec483a7b2a21c2a
Mohit Maithani <mohitmaithani81@gmail.com>
2018-07-25 11:40:21 +0530

    done

--- 2.pygame/snake.py
+++ 2.pygame/snake.py
@@ -1,12 +1,13 @@
-import pygame, sys
-from pygame.locals import *
+# import pygame, sys
+# from pygame.locals import *
 
-pygame.init()
-DISPLAYSURF = pygame.display.set_mode((800,300))
-pygame.display.set_caption('Mohit first pygame!')
-while True: # main game loop
-	for event in pygame.event.get():
-		if event.type == QUIT:
-			pygame.quit()
-			sys.exit()
-	pygame.display.update()
+# pygame.init()
+# DISPLAYSURF = pygame.display.set_mode((800,300))
+# pygame.display.set_caption('Mohit first pygame!')
+# while True: # main game loop
+# 	for event in pygame.event.get():
+# 		if event.type == QUIT:
+# 			pygame.quit()
+# 			sys.exit()
+# 	pygame.display.update()
+import tensorflow as tf
